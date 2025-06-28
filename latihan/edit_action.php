fa593afeefefe2f24187378d334d0673fc49ee57
Muhammad Angga <152399446+Angga-UGM@users.noreply.github.com>
2025-06-28 23:26:59 +0700

    Update edit_action.php
    Menambahkan $

--- latihan/edit_action.php
+++ latihan/edit_action.php
@@ -1,25 +1,25 @@
 <?php
-include="koneksi.php";
+include "koneksi.php";
 
 $id = $_POST['id'];
 $nim = $_POST['nim'];
 $nama = $_POST['nama'];
 
-//Querry update
+// Query update
 $query = mysqli_query($koneksi, "UPDATE daftar_mahasiswa SET
-                                    nim = '$nim',
-                                    nama= '$nama'
-                                    WHERE id = '$id'");
+                                nim = '$nim',
+                                nama = '$nama'
+                                WHERE id = '$id'");
 
-if(query) {
+if($query) {  // Perbaikan: tambahkan $ sebelum query
     echo '<script>
-    alert ("UPDATE DATA BERHASIL");
+    alert("UPDATE DATA BERHASIL");
     document.location="index.php";
-    </script>'
+    </script>';
 } else {
     echo '<script>
-    alert ("GAGAL UPDATE DATA: '.mysqli_error($koneksi).'");
+    alert("GAGAL UPDATE DATA: '.mysqli_error($koneksi).'");
     document.location="index.php";
-    </script>'
+    </script>';
 }
 ?>
